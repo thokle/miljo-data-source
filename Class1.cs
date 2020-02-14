343c1aba0b80d71b28a80ca39faf783fea15d9a8
Thomas Kleist <thomas@kleist-it.com>
2020-02-14 19:37:38 +0100

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace miljo_data_source
+{
+    public class Class1
+    {
+    }
+}
